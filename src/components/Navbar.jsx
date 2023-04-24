a10469f486e26823cd3a9cb1077a63df038d3f68
Joyjeet <joyxbhp6@gmail.com>
2023-04-25 00:27:36 +0530

    New changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div>
         <div className='flex justify-center absolute w-full z-10'>
-            <img src={Heritage} alt="Not found" className='m-5 h-[2rem] w-[6rem] md:h-[5rem] md:w-[6rem]'  />
+            <img src={Heritage} alt="Not found" className='m-5 h-[4rem] w-[5rem] md:h-[5rem] md:w-[7rem]'  />
             <img src={Aec} alt="Not found" className='m-5 h-[3rem] w-[4rem] md:h-[5rem] md:w-[6rem]'  />
             <p className='my-auto px-5 font-extrabold font-logo text-white text-sm md:text-lg text-center'>6th “Inter College Competition on Prototype Design for Mankind” (ICCPDM)</p>
         </div>
