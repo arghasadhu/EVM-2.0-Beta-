b2ccc0d01d3c5d657f925e9ee2a8c23dd67c007a
Joyjeet <joyxbhp6@gmail.com>
2023-12-19 21:14:21 +0530

    New message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Aec from "../assets/Aec.png";
-import Heritage from "../assets/Heritage.png";
 
 const Navbar = () => {
   return (
     <div>
         <div className='flex justify-center absolute w-full z-10'>
-            <img src={Heritage} alt="Not found" className='m-5 h-[4rem] w-[5rem] md:h-[5rem] md:w-[7rem]'  />
             <img src={Aec} alt="Not found" className='m-5 h-[3rem] w-[4rem] md:h-[5rem] md:w-[6rem]'  />
-            <p className='my-auto px-5 font-extrabold font-logo text-white text-sm md:text-lg text-center'>6th “Inter College Competition on Prototype Design for Mankind” (ICCPDM)</p>
+            <p className='my-auto px-5 font-extrabold font-logo text-white text-sm md:text-lg text-center'>ASANSOL ENGINEERING COLLEGE</p>
         </div>
     </div>
   )
